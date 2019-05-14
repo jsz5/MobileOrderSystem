a98066dd1f2a2972cc4f94825ddc0ba43a377624
asia <j.szolomicka@gmail.com>
2019-05-14 02:31:47 +0200

    settings menu

--- app/src/main/java/com/example/mobileordersystem/HomeActivity.kt
+++ app/src/main/java/com/example/mobileordersystem/HomeActivity.kt
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.view.View
 import android.widget.Toast
 import com.example.mobileordersystem.authorization.LoginActivity
@@ -12,6 +15,8 @@ import com.example.mobileordersystem.customer.CustomerFragment
 import com.example.mobileordersystem.equipment.EquipmentFragment
 import com.example.mobileordersystem.order.OrderFragment
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
 
 
 class HomeActivity : AppCompatActivity() {
@@ -55,9 +60,35 @@ class HomeActivity : AppCompatActivity() {
         val homeItem = navView.menu.getItem(0)
         homeItem.isChecked = true
         onNavigationItemSelectedListener.onNavigationItemSelected(homeItem)
-
+        val drawerToggle:ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ){
+            override fun onDrawerClosed(view:View){
+                super.onDrawerClosed(view)
+            }
+            override fun onDrawerOpened(drawerView: View){
+                super.onDrawerOpened(drawerView)
+            }
+        }
+        // Configure the drawer layout to add listener and show icon on toolbar
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        // Set navigation view navigation item selected listener
+        navigation_view.setNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.passwordChange ->  Toast.makeText(applicationContext,"Zmień hasło", Toast.LENGTH_SHORT).show()
+                R.id.deleteAccount -> showDeleteAccountDialog()
+                R.id.logut ->logout()
+            }
+            // Close the drawer
+            drawer_layout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
-
     private fun openFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, fragment)
@@ -65,14 +96,37 @@ class HomeActivity : AppCompatActivity() {
         transaction.commit()
     }
 
-    fun logout(view: View) {
+    private fun logout() {
         AuthUI.getInstance()
             .signOut(this)
             .addOnCompleteListener {
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
             }
-
+    }
+    private fun deleteAccount(){
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.delete()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+    }
+    private fun showDeleteAccountDialog()
+    {
+        val builder = AlertDialog.Builder(this@HomeActivity)
+        builder.setTitle(R.string.delete_account)
+        builder.setMessage(R.string.alertDelete)
+        builder.setPositiveButton("Tak"){dialog, which ->
+           deleteAccount()
+        }
+        builder.setNegativeButton("Nie"){dialog, which ->
+            dialog.dismiss()
+        }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
 
