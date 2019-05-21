f5cbf3658fec76b6339c9c8c7c197cbfa85d2b7f
asia <j.szolomicka@gmail.com>
2019-05-21 23:00:19 +0200

    addOrder

--- app/src/main/java/com/example/mobileordersystem/customer/Customer.kt
+++ app/src/main/java/com/example/mobileordersystem/customer/Customer.kt
@@ -1,8 +1,12 @@
 package com.example.mobileordersystem.customer
 
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 class Customer(
     var customerId: String,
-    var orderId: String?=null,
+    var orderId: String? = null,
     var name: String,
     var surname: String,
     var companyName: String,
@@ -11,4 +15,24 @@ class Customer(
     var address: String,
     var telephone: Int
 ) {
+    constructor() : this(
+        "", " ", "", "", "",
+        0, "", "", 0
+    )
+
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "customerId" to customerId,
+            "orderId" to orderId,
+            "name" to name,
+            "surname" to surname,
+            "companyName" to companyName,
+            "NIP" to NIP,
+            "email" to email,
+            "address" to address,
+            "telephone" to telephone
+
+        )
+    }
 }
--- app/src/main/java/com/example/mobileordersystem/order/CreateOrderFragment.kt
+++ app/src/main/java/com/example/mobileordersystem/order/CreateOrderFragment.kt
@@ -1,14 +1,27 @@
 package com.example.mobileordersystem.order
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.FragmentManager
 import com.example.mobileordersystem.R
+import com.example.mobileordersystem.customer.Customer
+import com.example.mobileordersystem.equipment.Equipment
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_create_order.*
 
+
+
+
+
 class CreateOrderFragment: androidx.fragment.app.Fragment() {
+    private   val customerList: MutableList<Customer> = mutableListOf()
+    private lateinit var databaseReference: DatabaseReference
     companion object {
         fun newInstance(): CreateOrderFragment = CreateOrderFragment()
     }
@@ -18,7 +31,12 @@ class CreateOrderFragment: androidx.fragment.app.Fragment() {
 
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        databaseReference = FirebaseDatabase.getInstance().reference
         super.onActivityCreated(savedInstanceState)
+        setDatePickers()
+        setCustomerSpinner()
+    }
+    private fun setDatePickers(){
         rentalDateInput.setOnClickListener {
             val newFragment = DatePicker()
             val bundle = Bundle()
@@ -32,6 +50,35 @@ class CreateOrderFragment: androidx.fragment.app.Fragment() {
             bundle.putString("dateButton", "return")
             newFragment.arguments=bundle
             newFragment.show(fragmentManager as FragmentManager, "datePicker")
+        }
+    }
+    private fun setCustomerSpinner(){
+        getCustomerList()
+        var customersNames= arrayListOf<String>()
+        Log.i("drugi", customerList[0].name)
+        for (customer in customerList){
+            customersNames.add(customer.name+" "+customer.surname)
+        }
+        val colors = arrayListOf<String>("Red", "Blue", "White", "Yellow", "Black", "Green", "Purple", "Orange", "Grey")
+        val spinner: Spinner = customerInput
+        val spinnerArrayAdapter = ArrayAdapter<String>(activity, android.R.layout.simple_spinner_item, customersNames)
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) // The drop down view
+        spinner.adapter = spinnerArrayAdapter
+
+    }
+    private fun getCustomerList() {
+        AsyncTask.execute {
+            val equipmentListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    customerList.clear()
+                    dataSnapshot.children.mapNotNullTo(customerList) { it.getValue<Customer>(Customer::class.java) }
+                    Log.i("pierwszy", customerList[0].name)
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    println("loadPost:onCancelled ${databaseError.toException()}")
+                }
+            }
+            databaseReference.child("Customer").addListenerForSingleValueEvent(equipmentListener)
         }
     }
 
